3b4a1c5ca73f3aeb02bf3f2ed6fb75ced640e9bd
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2023-01-28 23:28:05 +0530

    Add files via upload

---
+++ tattoo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tattoo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tattoo.settings')
+
+application = get_asgi_application()
